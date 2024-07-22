e1be4dd7cc00f77d908863a87f52e9a8be04b969
Yuvraj Singh <yuwe18b@gmail.com>
2024-07-22 15:12:26 +0530

    update code

---
+++ yuvraj_singh_018/objects.java
@@ -0,0 +1,12 @@
+class Sum 
+{
+	public static void main(String[] s)
+	{
+		int a , b , sum;
+		String[] arg;
+        a = Integer.parseInt(arg[0]);
+		b = Integer.parseInt(arg[1]);
+		int Sum = a+b ;
+		System.out.println("sum is :");
+	}
+}
