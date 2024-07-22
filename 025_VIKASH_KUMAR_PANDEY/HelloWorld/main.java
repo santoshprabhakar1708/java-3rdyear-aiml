9df24434816f1c909437661150d4788eef5d3cfd
Vikash Kumar Pandey <vikash.pan2003@gmail.com>
2024-07-22 15:45:54 +0530

    modified : HelloWorld/main.java

--- 025_VIKASH_KUMAR_PANDEY/HelloWorld/main.java
+++ 025_VIKASH_KUMAR_PANDEY/HelloWorld/main.java
@@ -4,3 +4,7 @@ class main{
         System.out.println("Hello world");
     }
 }
+
+// Static method and variable can be can be accessed irrespctive og object 
+// Static keyword helps execute main method without declaring an object 
+// 
