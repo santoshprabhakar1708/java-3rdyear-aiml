bd8a75db9439537bc90e415397c33fdb7e74bd1d
Pratyusha-Mukherjee <mukherjeepratyusha26@gmail.com>
2024-07-22 19:55:57 +0530

    a

---
+++ A.java
@@ -0,0 +1,9 @@
+class A{
+ public static void main(String[] args){
+       System.out.println("Java is:-");
+       for (int i=0;i<6;i++){
+            System.out.println(" "+args[i]);
+            }
+       System.out.println("programming language");
+     }
+}
