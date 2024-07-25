f1d5a8dcf654aeb2c7d5907a00ee84cf280b30cd
Debarjun <debarjunthakur020@gmail.com>
2024-07-25 12:24:24 +0530

    Method Overloading

--- Overloading.java
+++ Overloading.java
@@ -1,24 +1,19 @@
 import java.io.DataInputStream;
 import java.io.IOException;
-
 class Room {
-    float length, breadth;
-    
+    float length, breadth;    
     Room(int a, int b) {
         length = a;
         breadth = b;
     }
-    
     Room(int a) {
         length = a;
         breadth = 100;
     }
-    
     float getdata() {
         return (length * breadth);
     }
 }
-
 class Overloading {
     @SuppressWarnings("deprecation")
     public static void main(String args[]) {
@@ -34,13 +29,11 @@ class Overloading {
             System.out.println("Error reading input: " + e.getMessage());
             return;
         }
-        
         Room room1 = new Room(l, b);
         area = room1.getdata();
         System.out.println("\nLength = " + room1.length);
         System.out.println("Breadth = " + room1.breadth);
         System.out.println("Area = " + area);
-        
         System.out.println("\nPassing only length of the room with breadth = 100");
         Room room2 = new Room(l);
         area = room2.getdata();
